dcd56584084d083048a66efb92543d1dfde9654e
Jaman-M <jaman.manik1@gmail.comm>
2022-05-08 20:45:10 +0600

    create login and register form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import ProductDetail from './Pages/ProductDetail/ProductDetail';
 import Header from './Pages/Shared/Header/Header';
 import NotFound from './Pages/Shared/NotFound/NotFound';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/product/:productId' element={<ProductDetail></ProductDetail>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+    const handleRegister = event =>{
+        event.preventDefault();
+    }
     return (
-        <div>
-            <h2>please register</h2>
+        <div className='register-form mt-5'>
+            <h2 className=''>Please register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your email Address' required/>
+                <input type="password" name="password" id="" placeholder='password' required/>
+                <input type="checkbox" name="terms" id="terms" />
+                <label htmlFor="terms">Accept Terms and conditions</label>
+                <input className='w-50 mx-auto btn mt-2 mb-2 btn-primary ' type="submit" value="Register" />
+            </form>
+            <p>Already have an account ? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+            
         </div>
     );
 };
